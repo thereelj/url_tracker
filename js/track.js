b1b52dde98a7d140f93dd18688a59e8ac4291e90
Jez <jyanez@skygo.com.ph>
2022-05-26 16:57:00 +0800

    feat: get the value from input when clicking save button

--- js/track.js
+++ js/track.js
@@ -1,6 +1,9 @@
-let inputBtn = document.querySelector(".input-btn")
+const inputBtn = document.querySelector(".input-btn")
+const inputEl = document.querySelector(".input-el")
+let myLeads = []
 
 inputBtn.addEventListener("click", function() {
-    console.log("Button click event listener")
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 })
 
