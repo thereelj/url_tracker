d8b27d71b0103579b609994268b44d7045da71d3
Jez <jyanez@skygo.com.ph>
2022-05-26 17:41:29 +0800

    chore: pactice using innerHTML

--- js/track.js
+++ js/track.js
@@ -1,9 +1,19 @@
 const inputBtn = document.querySelector(".input-btn")
 const inputEl = document.querySelector(".input-el")
-let myLeads = []
+const ulEl = document.querySelector(".ul-el")
+let myLeads = ["www.facebook.com", "www.google.com", "www.linkedin.com"]
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
 })
 
+let listItems = ""
+
+for (let i = 0; i < myLeads.length; i++) {
+    // const li = document.createElement("li")
+    // li.textContent = myLeads[i]
+    // ulEl.append(li)
+    listItems += "<li>" + myLeads[i] + "</li>"
+}
+
+ulEl.innerHTML = listItems
