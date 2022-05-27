8f081a391d2a04a9119d20d5f77348781e3c6d43
Jez <jyanez@skygo.com.ph>
2022-05-27 11:51:07 +0800

    refactor: improve styling

--- js/track.js
+++ js/track.js
@@ -1,19 +1,27 @@
 const inputBtn = document.querySelector(".input-btn")
 const inputEl = document.querySelector(".input-el")
 const ulEl = document.querySelector(".ul-el")
-let myLeads = ["www.facebook.com", "www.google.com", "www.linkedin.com"]
+let myLeads = []
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
 })
 
-let listItems = ""
-
-for (let i = 0; i < myLeads.length; i++) {
-    // const li = document.createElement("li")
-    // li.textContent = myLeads[i]
-    // ulEl.append(li)
-    listItems += "<li>" + myLeads[i] + "</li>"
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        // const li = document.createElement("li")
+        // li.textContent = myLeads[i]
+        // ulEl.append(li)
+        listItems += `
+        <li>
+            <a href='${myLeads[i]}' target='_blank'>
+                ${myLeads[i]}
+            </a>
+        </li>
+        `
+    }
+    ulEl.innerHTML = listItems
 }
-
-ulEl.innerHTML = listItems
